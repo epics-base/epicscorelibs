45b8352a52fb936af665db8ab7761d6848177f7b
Michael Davidsaver <mdavidsaver@gmail.com>
2018-10-25 13:05:18 -0700

    7.0.1.99.15.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from epicscorelibs.config import get_config_var
 
 # our choice of version suffix is constrained by PEP 440
 # so we always append .99.ABI.SRC to most recent upstream version
-package_version = '7.0.1.99.15.1'
+package_version = '7.0.1.99.15.2'
 
 assert package_version.split('.')[-3]=='99', package_version
 
