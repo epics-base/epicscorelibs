bbdee3ac1263b5402d9cfd0f39663e7afd2197dc
AlexWells <alex.wells@diamond.ac.uk>
2023-10-25 13:59:39 +0100

    Only print warning when using old cothread version
    Everything from cothread 2.16+ is safe to use alongside epicscorelibs.

--- src/python/epicscorelibs/path/cothread.py
+++ src/python/epicscorelibs/path/cothread.py
@@ -14,8 +14,23 @@ import os, sys, warnings
 
 from . import get_lib
 
-if 'cothread.load_ca' in sys.modules or 'cothread.catools' in sys.modules:
-    warnings.warn("epicscorelibs.path.cothread must be imported before cothread.catools to have effect")
+if "cothread" in sys.modules:
+    # Ensure we don't `import cothread` ourselves as that can have side-effects
+    cothread = sys.modules["cothread"]
+    if hasattr(cothread, "_version"):
+        # This attribute only exists in cothread 2.19+.
+        # It is always safe to use alongside epicscorelibs
+        pass
+    else:
+        # Some cothread versions only include major and minor, no build version
+        version_components = cothread.__version__.split(".")
+        major = int(version_components[0])
+        minor = int(version_components[1])
+        if major >= 2 and minor >=16:
+            # Cothread 2.16 was the first to fallback to epicscorelibs' get_lib functionality
+            pass
+        else:
+            warnings.warn("epicscorelibs.path.cothread must be imported before cothread.catools to have effect")
 
 # don't override if already set
 if 'CATOOLS_LIBCA_PATH' not in os.environ:
