0733beae50157c9a90f8a223cc0eb47fce877506
Andrew Johnson <anj@anl.gov>
2025-02-20 10:27:41 -0600

    Fix for PR #597 breakage of VS-2012/2010 builds

--- modules/database/src/ioc/rsrv/camsgtask.c
+++ modules/database/src/ioc/rsrv/camsgtask.c
@@ -163,11 +163,11 @@ void camsgtask ( void *pParm )
 
 int casClientInitiatingCurrentThread ( char * pBuf, size_t bufSize )
 {
+    struct client * pClient;
     if ( ! rsrvCurrentClient )
         return RSRV_ERROR; /* not yet initialized, or disabled via dbServer */
-    struct client * pClient = ( struct client * )
-        epicsThreadPrivateGet ( rsrvCurrentClient );
 
+    pClient = ( struct client * ) epicsThreadPrivateGet ( rsrvCurrentClient );
     if ( ! pClient )
         return RSRV_ERROR;
 
